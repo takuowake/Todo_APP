e8f1db4baa54714376a6742ecf063200ae473186
takuowake <takuowake@gmail.com>
2021-04-29 01:51:24 +0000

    fix TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Todo;
+use App\Goal;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
@@ -14,17 +16,8 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        return response()->json($todos);
     }
 
     /**
@@ -35,29 +28,15 @@ class TodoController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Todo $todo)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Todo $todo)
-    {
-        //
+        $todo = new Todo();
+        $todo->content = request('content');
+        $todo->user_id = Auth::id();
+        $todo->goal_id = $goal->id;
+        $todo->position = request('position');
+        $todo->done = false;
+        $todo->save();
+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        return response()->json($todos);
     }
 
     /**
@@ -69,7 +48,15 @@ class TodoController extends Controller
      */
     public function update(Request $request, Todo $todo)
     {
-        //
+        $todo->content = request('content');
+        $todo->user_id = Auth::id();
+        $todo->goal_id = $goal->id;
+        $todo->position = request('position');
+        $todo->done = (bool) request('done');
+        $todo->save();
+
+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        return response()->json($todos);
     }
 
     /**
@@ -80,6 +67,26 @@ class TodoController extends Controller
      */
     public function destroy(Todo $todo)
     {
-        //
+        $todo->delete();
+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+        return response()->json($todos);
+    }
+    
+    public function sort(Request $request, Goal $goal, Todo $todo)
+    {
+        $exchangeTodo = Todo::where('position', request('sortId'))->first();
+        $lastTodo = Todo::where('position', request('sortId'))->latest('position')->first();
+
+        if (request('sortId') == 0) {
+            $todo->moveBefore($exchangeTodo);
+        } else if (request('sortId') - 1 == $lastTodo->position) {
+            $todo->moveAfter($exchangeTodo);
+        } else {
+            $todo->moveAfter($exchangeTodo);
+        }
+
+        $todos = $goal->todos()->orderBy('done', 'asc')->orderBy('position', 'asc')->get();
+
+        return response()->json($todos);
     }
 }
